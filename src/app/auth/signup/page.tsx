dbcae81901c830074d7e21deacde579d6c9720f2
Phan Duy Viet Vo <vophanduyviet@gmail.com>
2025-04-26 00:05:16 +1000

    Auth UI's components added.

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,88 +1,9 @@
-"use client";
+import SignUp from "src/app/auth/signup/components/SignUp";
 
-import { useState } from "react";
-import Link from "next/link";
-import { Eye, EyeOff } from "lucide-react";
-import PrimaryButton from "../components/PrimaryButton";
-
-
-const SignupForm = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [showPassword, setShowPassword] = useState(false);
-
-    const handleSignup = (e: React.FormEvent) => {
-        e.preventDefault();
-    };
-
+export default function Booking() {
     return (
-        <div className="flex justify-center items-center grow bg-white">
-            <form
-                onSubmit={handleSignup}
-                className="w-full max-w-lg md:max-w-3xl space-y-6 px-6 py-10 shadow-md rounded-md"
-            >
-                <h2 className="text-2xl font-bold text-gray-900">Create an account</h2>
-
-                <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="email">
-                        Email
-                    </label>
-                    <input
-                        id="email"
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                        className="w-full border border-blue-100 rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-blue-300"
-                        placeholder="example@gmail.com"
-                    />
-                </div>
-
-                <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1" htmlFor="password">
-                        Password
-                    </label>
-                    <div className="relative">
-                        <input
-                            id="password"
-                            type={showPassword ? "text" : "password"}
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                            className="w-full border border-gray-300 rounded-md px-4 py-2 pr-10 outline-none focus:ring-2 focus:ring-blue-300"
-                            placeholder="Enter your password"
-                        />
-                        <button
-                            type="button"
-                            onClick={() => setShowPassword((prev) => !prev)}
-                            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500"
-                            aria-label="Toggle password visibility"
-                        >
-                            {showPassword ? <Eye className="w-5 h-5" /> : <EyeOff className="w-5 h-5" />}
-                        </button>
-                    </div>
-                </div>
-
-                <PrimaryButton type="submit">Create account</PrimaryButton>
-
-                <button className="w-full bg-blue-100 text-gray-800 py-2 rounded-md font-medium flex items-center justify-center gap-2 hover:bg-blue-200 transition">
-                    <img
-                        src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
-                        alt="Google"
-                        className="w-5 h-5"
-                    />
-                    Continue with Google
-                </button>
-
-                <p className="text-center text-sm text-gray-600">
-                    Already Have An Account ?{" "}
-                    <Link href="/auth/login" className="text-blue-600 hover:underline">
-                        Log In
-                    </Link>
-                </p>
-            </form>
+        <div className="pt-12">
+            <SignUp/>
         </div>
     );
-};
-
-export default SignupForm;
+}
--- src/app/auth/verify/page.tsx
+++ src/app/auth/verify/page.tsx
@@ -1,49 +1,9 @@
-"use client";
+import Verify from "src/app/auth/verify/components/Verify";
 
-import { useState } from "react";
-import PrimaryButton from "../components/PrimaryButton";
-import Link from "next/link";
-
-const VerifyCodeForm = () => {
-    const [code, setCode] = useState("");
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-    };
-
+export default function Booking() {
     return (
-        <div className="flex justify-center items-center grow bg-white">
-            <form
-                onSubmit={handleSubmit}
-                className="w-full max-w-lg md:max-w-2xl space-y-4 px-6 py-8 shadow-md rounded-md"
-            >
-                <h2 className="text-2xl font-bold text-gray-900">Verify code</h2>
-
-                <p className="text-medium text-gray-700">
-                    An Authentication Code Has Been Sent To Your Email. Enter Code:
-                </p>
-
-                <input
-                    id="code"
-                    type="text"
-                    value={code}
-                    onChange={(e) => setCode(e.target.value)}
-                    required
-                    className="w-full border border-blue-100 rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-blue-300"
-                    placeholder="123ABC"
-                />
-
-                <PrimaryButton type="submit">Verify</PrimaryButton>
-
-                <p className="text-center text-medium text-gray-400">
-                    Didn't Receive A Code?{" "}
-                    <Link href="#" className="text-blue-600 hover:underline">
-                        Resend
-                    </Link>
-                </p>
-            </form>
+        <div className="pt-12">
+            <Verify/>
         </div>
     );
-};
-
-export default VerifyCodeForm;
+}
