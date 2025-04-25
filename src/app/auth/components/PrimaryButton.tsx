4ad09df643952d7b2323bb744dbac1085edeee47
Phan Duy Viet Vo <vophanduyviet@gmail.com>
2025-04-25 17:12:59 +1000

    Auth page UI

---
+++ src/app/auth/components/PrimaryButton.tsx
@@ -0,0 +1,25 @@
+type PrimaryButtonProps = {
+    children: React.ReactNode;
+    type?: "button" | "submit" | "reset";
+    onClick?: () => void;
+    className?: string;
+};
+
+const PrimaryButton = ({
+                           children,
+                           type = "button",
+                           onClick,
+                           className = "",
+                       }: PrimaryButtonProps) => {
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            className={`w-full bg-[#000057] text-white py-3 rounded-md font-semibold hover:bg-blue-900 transition ${className}`}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default PrimaryButton;
