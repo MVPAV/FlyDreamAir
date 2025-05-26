a40c88daf8808af3788cfd12c2b019fb344d3134
notabota <nnv2205owo@gmail.com>
2025-05-27 03:53:10 +1000

    Add auth modal

--- src/app/auth/signin/components/SignIn.tsx
+++ src/app/auth/signin/components/SignIn.tsx
@@ -6,14 +6,14 @@ import {Eye, EyeOff} from "lucide-react";
 import {useRouter} from "next/navigation";
 import {signIn} from "next-auth/react";
 
-const SigninForm = () => {
+const SignInForm = () => {
     const router = useRouter();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [showPassword, setShowPassword] = useState(false);
     const [error, setError] = useState("");
 
-    const handleSignin = async (e: React.FormEvent) => {
+    const handleSignIn = async (e: React.FormEvent) => {
         e.preventDefault();
 
         if (!email || !password) {
@@ -42,10 +42,10 @@ const SigninForm = () => {
     return (
         <div className="py-20 flex justify-center items-center grow bg-white">
             <form
-                onSubmit={handleSignin}
+                onSubmit={handleSignIn}
                 className="w-full max-w-lg md:max-w-3xl space-y-6 px-6 py-10 shadow-md rounded-md"
             >
-                <h2 className="text-2xl font-bold text-gray-900">Signin to your account</h2>
+                <h2 className="text-2xl font-bold text-gray-900">Sign in to your account</h2>
 
                 <div>
                     <label className="block text-lg font-medium text-gray-700 mb-1" htmlFor="email">
@@ -86,7 +86,7 @@ const SigninForm = () => {
                         </button>
                     </div>
                     <div className="text-right mt-1">
-                        <Link href="/auth/forgot_password" className="text-medium text-blue-600 hover:underline">
+                        <Link href="/auth/forgot-password" className="text-medium text-blue-600 hover:underline">
                             Forgot Password ?
                         </Link>
                     </div>
@@ -97,6 +97,16 @@ const SigninForm = () => {
                     className="text-lg w-full bg-[#000057] text-white py-2 rounded-md font-semibold hover:bg-blue-900"
                 >
                     Sign in now
+                </button>
+
+                <button
+                    className="text-lg w-full bg-blue-100 text-gray-800 py-2 rounded-md font-medium flex items-center justify-center gap-2 hover:bg-blue-200 transition">
+                    <img
+                        src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+                        alt="Google"
+                        className="w-5 h-5"
+                    />
+                    Continue with Google
                 </button>
 
                 <p className="text-center text-sm text-gray-600">
@@ -110,4 +120,4 @@ const SigninForm = () => {
     );
 };
 
-export default SigninForm;
+export default SignInForm;
