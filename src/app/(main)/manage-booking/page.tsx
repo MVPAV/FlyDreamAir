1a4e81c09df9e9b71429b2223c8b9f5b4260664a
notabota <nnv2205owo@gmail.com>
2025-05-27 10:32:40 +1000

    Add suspense to manage booking

--- src/app/(main)/manage-booking/page.tsx
+++ src/app/(main)/manage-booking/page.tsx
@@ -1,11 +1,14 @@
 import ManageBooking from "src/app/(main)/manage-booking/components/ManageBooking";
 import TravelToolsSection from "src/app/(main)/manage-booking/components/TravelToolsSection";
+import {Suspense} from "react";
 
 export default function Booking() {
     return (
         <div className="pt-12">
-            <ManageBooking/>
-            <TravelToolsSection/>
+            <Suspense fallback={<h1>Loading</h1>}>
+                <ManageBooking/>
+                <TravelToolsSection/>
+            </Suspense>
         </div>
     );
 }
